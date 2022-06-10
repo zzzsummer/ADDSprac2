5a5c8b2253691227210591cd968eea851f6d34b6
zzzsummer <83861243+zzzsummer@users.noreply.github.com>
2022-06-10 18:56:01 +0800

    Update Human.h

--- Human.h
+++ Human.h
@@ -3,31 +3,8 @@
 class Human
 {
 public:
-    Human(){
-
-    }
-    char makeMove(){
-        char move;
-        std::cout << "Enter move\n";
-        std::cin >> move;
-        if (move == 'r' || move == 'R'){
-            return 'r';
-        }
-        else if (move == 'p' || move == 'P'){
-            return 'p';
-        }
-        else if (move == 's' || move == 'S'){
-            return 's';
-        }
-        else{
-            std::cout << "Invalid move. Try again.\n";
-            return makeMove();
-        }
-    }
-
-    char getMove(){
-        return move;
-    }
+    Human();
+    char makeMove();
 
 private:
     char move;
