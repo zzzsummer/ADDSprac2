ad77aa62a856c1190222be1e1d20e3efa0516ade
zzzsummer <83861243+zzzsummer@users.noreply.github.com>
2022-06-10 18:57:26 +0800

    Update Referee.h

--- Referee.h
+++ Referee.h
@@ -6,26 +6,24 @@ class Referee
 public:
     Referee(){
 
-    };
-    char Game(Human* human, Computer* computer){
-        char humanMove = human->makeMove();
-        char computerMove = computer->makeMove();
-        if (humanMove == 'r' && computerMove == 's'){
+    }
+    char refGame(Human player1,Computer player2){
+        char player1Move = player1.makeMove();
+        char player2Move = player2.makeMove();
+        if (player1Move == 'r' && player2Move == 's'){
             return 'W';
         }
-        else if (humanMove == 's' && computerMove == 'p'){
+        else if (player1Move == 's' && player2Move == 'p'){
             return 'W';
         }
-        else if (humanMove == 'p' && computerMove == 'r'){
+        else if (player1Move == 'p' && player2Move == 'r'){
             return 'W';
         }
-        else if (humanMove == computerMove){
+        else if (player1Move == player2Move){
             return 'T';
         }
         else{
             return 'L';
         }
-    };
-};
-    
-
+    }
+};   
