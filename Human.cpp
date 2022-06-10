39b015bab537ebd6f6d1650debe382953e7776fd
zzzsummer <83861243+zzzsummer@users.noreply.github.com>
2022-06-10 18:56:37 +0800

    Update Human.cpp

--- Human.cpp
+++ Human.cpp
@@ -1,2 +1,24 @@
 #include "Human.h"
 
+Human::Human()
+{
+}
+
+char Human::makeMove(){
+        char move;
+        std::cout << "Enter move\n";
+        std::cin >> move;
+        if (move == 'r' || move == 'R'){
+            return 'r';
+        }
+        else if (move == 'p' || move == 'P'){
+            return 'p';
+        }
+        else if (move == 's' || move == 'S'){
+            return 's';
+        }
+        else{
+            std::cout << "Invalid move. Try again.\n";
+            return makeMove();
+        }
+    }
