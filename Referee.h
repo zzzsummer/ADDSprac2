9dcb66007604ecbffd40d198508caad60c33a127
zzzsummer <83861243+zzzsummer@users.noreply.github.com>
2022-06-10 19:05:58 +0800

    Update Referee.h

--- Referee.h
+++ Referee.h
@@ -10,20 +10,19 @@ public:
     char refGame(Human player1,Computer player2){
         char player1Move = player1.makeMove();
         char player2Move = player2.makeMove();
-        if (player1Move == 'r' && player2Move == 's'){
-            return 'W';
-        }
-        else if (player1Move == 's' && player2Move == 'p'){
-            return 'W';
-        }
-        else if (player1Move == 'p' && player2Move == 'r'){
-            return 'W';
-        }
-        else if (player1Move == player2Move){
+        if (player1Move == 'r'){
             return 'T';
         }
-        else{
+        else if (player1Move == 's'){
             return 'L';
         }
+        else if (player1Move == 'p' ){
+            return 'W';
+        }
+       
     }
 };   
+
+
+
+    
